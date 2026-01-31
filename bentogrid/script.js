0e7042f6e87b3c2e4a30212a16ee923ce7a1f82b
Harshit Raghuwanshi <Harshuraghu7999@gmail.com>
2026-01-31 19:40:44 +0530

    moved the folder

--- bentogrid/script.js
+++
@@ -1,8 +0,0 @@
-let circle = document.querySelector(".circle")
-let text = document.querySelector("h1")
-
-function clickit(){
-    text.textContent = "NO"
-}
-
-circle.addEventListener("click",  clickit)
